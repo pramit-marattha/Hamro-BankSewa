f23195dd5d5c303a79329ed7fa39ee8ac9d9df6e
pramit-marattha <pramitmarattha5497@gmail.com>
2021-04-03 01:04:12 +0545

    hamburger menu animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-console.log("Yolo test tes");
+const hamburgerButton = document.querySelector(".header__hamburgerMenu");
+
+hamburgerButton.addEventListener("click", () => {
+  console.log("button clicked");
+  let classHamCheck = hamburgerButton.classList.contains("open");
+
+  if (classHamCheck) {
+    hamburgerButton.classList.remove("open");
+  } else {
+    hamburgerButton.classList.add("open");
+  }
+});
